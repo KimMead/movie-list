8e2602d52e3816646ffdf12ac526513fa5aa97f3
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-01-25 23:55:36 +0000

    Added login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,18 @@ class UsersController < ApplicationController
       redirect to '/movies'
     end
   end
+
+  get '/users/login' do
+      erb :'users/login'
+    end
+
+  post '/login' do
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if !@user
+      redirect to '/users/login'
+    else
+      session[:user_id] = @user.id
+      redirect to '/movies'
+     end
+   end
 end
