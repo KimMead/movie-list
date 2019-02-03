012e98b593c5747f2398de7b7931ceb93232769a
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-03 20:19:10 +0000

    Add user only edit functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,13 +23,29 @@ end
 
   get '/movies/:id/edit' do
     @movie = Movie.find(params[:id])
-    erb :'movies/edit'
+    if logged_in?
+      if @movie.user == current_user
+        erb :'movies/edit'
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else
+    redirect '/'
+    end
   end
 
   patch '/movies/:id' do
     @movie = Movie.find(params[:id])
+    if logged_in?
+      if @movie.user == current_user
     @movie.update(title: params[:title], release_date: params[:release_date])
-    redirect to ("/movies/#{@movie.id}")
+    redirect to "/movies/#{@movie.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else
+    redirect '/'
+    end
   end
 
   get '/movies' do
